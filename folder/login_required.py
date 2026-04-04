f78bd38cf2368c171761b638e4ead413c78b5b21
Lizok2872 <lizavladimirova1120@gmail.com>
2026-04-05 00:38:59 +0300

    Update login_required.py

--- folder/login_required.py
+++ folder/login_required.py
@@ -32,3 +32,42 @@ def new_recipe():
         return redirect(url_for('index'))
 
     return render_template('recipe_form.html', form=form, title='Добавить рецепт')
+
+
+@app.route('/recipe/<int:recipe_id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+
+    if recipe.user_id != current_user.id:
+        flash('У вас нет прав для редактирования этого рецепта', 'danger')
+        return redirect(url_for('index'))
+
+    form = RecipeForm(obj=recipe)
+    if form.validate_on_submit():
+        recipe.title = form.title.data
+        recipe.description = form.description.data
+        recipe.ingredients = form.ingredients.data
+        recipe.instructions = form.instructions.data
+        recipe.cooking_time = form.cooking_time.data
+        recipe.category = form.category.data
+        db.session.commit()
+        flash('✏ Рецепт успешно обновлен!', 'success')
+        return redirect(url_for('view_recipe', recipe_id=recipe.id))
+
+    return render_template('recipe_form.html', form=form, title='Редактировать рецепт')
+
+
+@app.route('/recipe/<int:recipe_id>/delete', methods=['POST'])
+@login_required
+def delete_recipe(recipe_id):
+    recipe = Recipe.query.get_or_404(recipe_id)
+
+    if recipe.user_id != current_user.id:
+        flash('У вас нет прав для удаления этого рецепта', 'danger')
+        return redirect(url_for('index'))
+
+    db.session.delete(recipe)
+    db.session.commit()
+    flash('🗑 Рецепт успешно удален!', 'success')
+    return redirect(url_for('index'))
