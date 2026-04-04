aea0dcc9687e0a876485395108efae04ddec05e5
Lizok2872 <lizavladimirova1120@gmail.com>
2026-04-05 00:45:05 +0300

    Update login_manager.py

--- folder/login_manager.py
+++ folder/login_manager.py
@@ -1,7 +1,8 @@
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-with app.app_context():
-    db.create_all()
+class Login_manager:
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    
+    with app.app_context():
+        db.create_all()
