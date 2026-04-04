55ccceb0188e57195299ae0129321df88c9a6778
Lizok2872 <lizavladimirova1120@gmail.com>
2026-04-05 00:41:38 +0300

    Update errorhandler.py

--- folder/errorhandler.py
+++ folder/errorhandler.py
@@ -1,9 +1,9 @@
-class Errorhandler:
-    @app.errorhandler(404)
-        def not_found(error):
-            return make_response(jsonify({'error': 'Not found'}), 404)
-    
-    
-    @app.errorhandler(400)
-    def bad_request(error):
-        return make_response(jsonify({'error': 'Bad Request'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
